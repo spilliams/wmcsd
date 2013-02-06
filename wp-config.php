310882dc87d121a86b859525149d46b63d7edf3f
spilliams <s@spencerenglish.com>
2013-02-05 23:19:25 -0800

    settings

--- wp-config.php
+++ wp-config.php
@@ -20,3 +20,41 @@ define('LOGGED_IN_SALT',   ' 2;|gdE5[Z2/rG:h5a#_XUez;VLJ^14][)2/E-X<uPJqfJ_`)|-6
 define('NONCE_SALT',       '%_flqka5e/RR<_1;cbeJ0lsl8.+d|u9l_,J|UV0P1KXi GlRKA_y.e.b^w.rD:>D');
 
 define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] );
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('WPLANG', '');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
