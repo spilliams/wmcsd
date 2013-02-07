abb7b972c5adbe19695126f9f4740e339e88b5f7
spilliams <s@spencerenglish.com>
2013-02-06 21:52:36 -0800

    page templates, continued

--- wp-content/themes/williamsmeyer/content-page.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package WilliamsMeyer
- * @since Williams & Meyer 1.0
- */
-?>
-
-content-page
--- wp-content/themes/williamsmeyer/page.php
+++ wp-content/themes/williamsmeyer/page.php
@@ -6,7 +6,6 @@
 
 get_header(); ?>
 
-page
 <?php while ( have_posts() ) : the_post(); ?>
   <?php get_template_part( 'content', 'page' ); ?>
 <?php endwhile; // end of the loop. ?>
