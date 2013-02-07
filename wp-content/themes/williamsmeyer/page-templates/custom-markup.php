e8bec41a19d9e018f4bca8c1a69c29f3de3171f4
spilliams <s@spencerenglish.com>
2013-02-06 19:56:53 -0800

    page templates!

---
+++ wp-content/themes/williamsmeyer/page-templates/custom-markup.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Custom Markup Template
+ *
+ * Description: A page template that allows custom markup.
+ *
+ * @package WilliamsMeyer
+ * @since Williams & Meyer 1.0
+ */
+
+get_header(); ?>
+
+custom-markup-page-template
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/williamsmeyer/page-templates/default.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Default Template
+ *
+ * Description: The default page template. Grey.
+ *
+ * @package WilliamsMeyer
+ * @since Williams & Meyer 1.0
+ */
+
+get_header(); ?>
+
+default-page-template
+
+<?php get_footer(); ?>
