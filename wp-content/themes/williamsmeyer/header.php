a02fa7d19bbe844300d234a47d8228fc7ff7b2f7
Spencer Williams <s@spencerenglish.com>
2013-02-07 21:28:55 -0800

    header...

--- wp-content/themes/williamsmeyer/header.php
+++ wp-content/themes/williamsmeyer/header.php
@@ -43,6 +43,7 @@
     <div class="grey">
       <div class="row">
         <div class="three columns"><h2><a href="/">Posts</a></h2></div>
-        <div class="three columns last"><h2><a href="/about">About</a></h2></div>
+        <div class="three columns end"><h2><a href="/about">About</a></h2></div>
+        <hr />
       </div>
     </div>
