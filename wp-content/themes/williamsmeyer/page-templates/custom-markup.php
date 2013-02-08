cd99af87b7654562d7995753f9143c5e76b929ff
Spencer Williams <s@spencerenglish.com>
2013-02-07 20:10:56 -0800

    for the about page

--- wp-content/themes/williamsmeyer/page-templates/custom-markup.php
+++ wp-content/themes/williamsmeyer/page-templates/custom-markup.php
@@ -10,6 +10,8 @@
 
 get_header(); ?>
 
-<?php the_content(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post();?>
+  <?php the_content(); ?>
+<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
