9f58e6517235ad9e40f0a874b660edab58239117
Spencer Williams <s@spencerenglish.com>
2013-02-10 10:48:06 -0800

    attempt at media fix

--- wp-content/themes/williamsmeyer/README.md
+++ wp-content/themes/williamsmeyer/README.md
@@ -19,13 +19,12 @@ Williams &amp; Meyer, and the Corps of Self-Discovery
 - About page (/all pages)
     - not readable enough (blue and orange)
     - pictures should blow up in a lightbox (don't do extra tabs--takes the user outside the experience)
-- moar templates
-    - video post
-    - image gallery
-    - should these even be separate types of post? That's one more thing for admin to curate.
-- mobile?
-- more WP-BP
-    - for instance: "Captains" supertitle editable, banner, footer widgets
-    - sidebar?
-    - search?
+- moar stylez
+    - images
+    - videos
+    - large blocks of text
+- mobile
+    - title card smaller
+    - nav less obtrusive
 - load bg image asynchronously or compress it or something
+- favicon
---
+++ wp-content/themes/williamsmeyer/functions.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Williams & Meyer functions and definitions.
+ *
+ * @package WilliamsMeyer
+ * @since Williams & Meyer 1.0
+ */
+ 
+/**
+ * Sets up theme defaults and registers the various WordPress features that
+ * Twenty Twelve supports.
+ *
+ * @uses add_theme_support() To add support for post thumbnails, automatic feed links,
+ * 	custom background, and post formats.
+ * @uses set_post_thumbnail_size() To set a custom post thumbnail size.
+ *
+ * @since Williams & Meyer 1.0
+ */
+function williamsmeyer_setup() {
+	// Adds RSS feed links to <head> for posts and comments.
+	add_theme_support( 'automatic-feed-links' );
+
+	// This theme supports a variety of post formats.
+	add_theme_support( 'post-formats', array( 'aside', 'image', 'link', 'quote', 'status' ) );
+
+	/*
+	 * This theme supports custom background color and image, and here
+	 * we also set up the default background color.
+	 */
+	add_theme_support( 'custom-background', array(
+		'default-color' => 'e6e6e6',
+	) );
+
+	// This theme uses a custom image size for featured images, displayed on "standard" posts.
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 890, 550 ); // Unlimited height, soft crop
+}
+add_action( 'after_setup_theme', 'williamsmeyer_setup' );
