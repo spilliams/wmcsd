4c17af213037f4fd6eea02c51780c6fa4b34bbb4
Spencer Williams <s@spencerenglish.com>
2013-03-22 19:25:31 -0700

    rss link, beginning of social foundicons

--- wp-content/themes/williamsmeyer/header.php
+++ wp-content/themes/williamsmeyer/header.php
@@ -19,6 +19,7 @@
 
   <link href='http://fonts.googleapis.com/css?family=IM+Fell+DW+Pica:400,400italic|Flamenco:400' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/stylesheets/foundation.min.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/stylesheets/social_foundicons.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
   
   <?php wp_head(); ?>
@@ -46,6 +47,7 @@
       <div class="row">
         <div class="three columns text-center"><h2><a href="/">Posts</a></h2></div>
         <div class="three columns text-center"><h2><a href="/about/">About</a></h2></div>
-        <div class="three columns text-center end"><h2><a href="/contact/">Contact</a></h2></div>
+        <div class="three columns text-center"><h2><a href="/contact/">Contact</a></h2></div>
+        <div class="three columns text-center"><h2><a href="/feed/">RSS</a></h2></div>
       </div>
     </div>
