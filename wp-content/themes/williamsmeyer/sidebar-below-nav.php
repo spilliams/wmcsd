b94e4702e8798a9808d83e1b8bc61fd50907f069
spilliams <s@spencerenglish.com>
2013-04-05 00:04:38 -0700

    countdown

--- wp-content/themes/williamsmeyer/sidebar-below-nav.php
+++ wp-content/themes/williamsmeyer/sidebar-below-nav.php
@@ -1,5 +1,18 @@
-<div id="sidebar-below-nav" class="sidebar">
+<?php
+/**
+ * @package WilliamsMeyer
+ * @since Williams & Meyer 1.0
+ */
+/*
+ * If this sidebar has no widgets, then let's bail early.
+ */
+if ( ! is_active_sidebar( 'below-nav' ) )
+	return;
+
+// If we get this far, we have widgets. Let do this.
+?>
+<div id="sidebar-below-nav" class="sidebar grey"><div class="row"><div class="twelve columns">
   <?php if (function_exists('dynamic_sidebar')) : ?>
     <?php dynamic_sidebar('below-nav'); ?>
   <?php endif; ?>
-</div>
+</div></div></div>
